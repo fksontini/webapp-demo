99d4529fc4536e7003a26a9e267d53773f106c0e
Firas KSONTINI <firas.ksontini@gmail.com>
2017-01-18 19:06:32 +0100

    First commit

---
+++ Webapp/Controllers/EquipeController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Webapp.Models;
+
+namespace Webapp.Controllers
+{
+    public class EquipeController : Controller
+    {
+        // GET: Equipe
+        public ActionResult Index()
+        {
+            List<Equipe> list = Session["equipe"] as List<Equipe>;
+            return View(list);
+        }
+
+        // GET: Equipe/Details/5
+        public ActionResult Details(int id)
+        {
+
+            List<Equipe> list = Session["equipe"] as List<Equipe>;
+
+            Equipe eq = list.Where(a => a.EquipeIds == id).Single();
+
+            return View(eq);
+        }
+
+        // GET: Equipe/Create
+        public ActionResult Create()
+        {
+            List<Equipe> list = Session["equipe"] as List<Equipe>;
+
+            return View();
+        }
+
+        // POST: Equipe/Create
+        [HttpPost]
+        public ActionResult Create(Equipe equipe)
+        {
+            
+
+            try
+            {
+                List<Equipe> list = Session["equipe"] as List<Equipe>;
+                if (list == null)
+                {
+                    list = new List<Equipe>();
+                }
+                list.Add(equipe);
+                Session["equipe"] = list;
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Equipe/Edit/5
+        public ActionResult Edit(int id)
+        {
+            List<Equipe> list = Session["equipe"] as List<Equipe>;
+
+            Equipe eq = list.Where(a => a.EquipeIds == id).Single();
+            return View(eq);
+        }
+
+        // POST: Equipe/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Equipe equipe)
+        {
+            try
+            {
+                List<Equipe> list = Session["equipe"] as List<Equipe>;
+
+                Equipe eq = list.Where(a => a.EquipeIds == id).Single();
+                foreach (var item in list)
+                {if (item.EquipeIds == equipe.EquipeIds)
+                    {
+                        item.Nom = equipe.Nom;
+                        item.Pays = equipe.Pays;
+                        item.datecreation = equipe.datecreation;
+                    }
+                }
+                Session["equipe"] = list;
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Equipe/Delete/5
+        public ActionResult Delete(int id)
+        {
+            List<Equipe> list = Session["equipe"] as List<Equipe>;
+
+            Equipe eq = list.Where(a => a.EquipeIds == id).Single();
+            return View(eq);
+          
+        }
+
+        // POST: Equipe/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                List<Equipe> list = Session["equipe"] as List<Equipe>;
+
+                Equipe eq = list.Where(a => a.EquipeIds == id).Single();
+                list.Remove(eq);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Webapp/Models/Equipe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Webapp.Models
+{
+    public class Equipe
+    {
+        public int EquipeIds { get; set; }
+        public string Nom { get; set; }
+        public string Pays { get; set; }
+        [DataType(DataType.Date)]
+        [Display(Name  ="Date de création ")]
+        public DateTime datecreation { get; set; }
+    }
+}
